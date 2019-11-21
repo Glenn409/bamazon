b7a7148d29858e9bdeafb12620797e221a6f60bd
Chris Glenn <chrisglenn519@yahoo.com>
2019-11-21 16:35:40 -0600

    finished

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -21,10 +21,11 @@ connection.connect(function(err,res){
     ]).then(function(data){
         switch(data.choice){
             case "View Product Sales by Department":
-                viewDepartments();
-                break;
+                viewDepartments()
+                break
             case 'Create New Department':
-                break;
+                createDepartment()
+                break
         }
     })
 })
@@ -45,5 +46,28 @@ function viewDepartments(){
             console.log(`ID: ${data[i].department_id} Name: ${data[i].department_name} Over Head Costs: ${data[i].over_head_costs} Sales: ${data[i].Sales} Profit: ${data[i].Profit}`)
         }
         connection.end();
+    })
+}
+
+function createDepartment(){
+    inquire.prompt([
+        {
+            type:'input',
+            message: 'What department would you like to add?',
+            name: 'newDepartment'
+        },
+        {
+            type: 'input',
+            message: 'What are the overhead costs?',
+            name: 'costs'
+        }
+    ]).then(function(data){
+
+        connection.query(`INSERT INTO departments (department_name,over_head_costs) VALUES ("${data.newDepartment}","${data.costs}")`,
+        function(err,res){
+            if(err) throw err
+            console.log('Department Added!')
+            connection.end()
+        })
     })
 }
